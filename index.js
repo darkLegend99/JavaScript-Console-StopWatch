8e59ccd48d25baff741ff0cfc4a3592e67944ca5
darklegend99 <darklegend1931@gmail.com>
2021-06-15 14:17:34 +0530

    stopwatch

---
+++ index.js
@@ -0,0 +1,33 @@
+function Stopwatch() {
+    let startTime,endTime,running,duration=0;
+
+    this.start = function() {
+        if(running)
+            throw new Error('The StopWatch is already running!!');
+        running = true;
+        startTime = new Date();
+    };
+
+    this.stop = function() {
+        if(!running)
+            throw new Error('The StopWatch is now running. First start the stopWatch.');
+        running = false;
+        endTime = new Date();
+
+        duration = (endTime.getTime() - startTime.getTime())/1000;
+    };
+
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {
+            return duration;
+        }   
+    });
+}
+
